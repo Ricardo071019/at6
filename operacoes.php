333c40269b5909daec56047d692498004cd8f554
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:12:20 +0100

    Update operacoes.php

--- operacoes.php
+++ operacoes.php
@@ -9,3 +9,29 @@ $c = 20;
 $media = $a + $b + $c / 3;
 echo $a. "+" .$b."+" .$c."=" .$media."";
 
+echo "<br>";
+$x = 12;
+$y = 10;
+$quociente = $x + $y;
+$resto = ($x % $y);
+echo "<br><br>41/6=".$quociente;
+echo "<br>resto=".$resto;
+
+echo "<br><br>O João foi à oficina e a
+reparação do carro é de 78,00 € sem IVA.";
+$preco=78.00;
+$iva=0.50;
+$soma=$preco*$iva;
+$quociente=$preco+$iva+$soma;
+echo "<br> O João com iva paragara ".$quociente;
+?>  
+
+
+
+
+
+
+
+
+
+
